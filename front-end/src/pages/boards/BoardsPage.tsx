14eb307f021f5df1a08b6a26b6a8a229063daca2
MingCheng-Hsiao-1171871 <Ming-Cheng.Hsiao@lincolnuni.ac.nz>
2026-04-11 15:55:26 +1200

    Upgrade TailwindCSS from v3 tp v4 to match shadcn/ui

--- front-end/src/pages/boards/BoardsPage.tsx
+++ front-end/src/pages/boards/BoardsPage.tsx
@@ -1,8 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function BoardsPage() {
     return (
         <div>
             <h1>Boards</h1>
             <p>This is the boards page.</p>
+
+            <Button size="lg">Click me</Button>
         </div>
     );
 }

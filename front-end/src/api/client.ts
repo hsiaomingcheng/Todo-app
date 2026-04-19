e3f7c304a5a057936997dbc99d58332c12b6510c
MingCheng-Hsiao-1171871 <Ming-Cheng.Hsiao@lincolnuni.ac.nz>
2026-04-19 16:37:32 +1200

    feat: Add interceptor for expire token

--- front-end/src/api/client.ts
+++ front-end/src/api/client.ts
@@ -5,8 +5,9 @@ const api = axios.create({
   headers: { "Content-Type": "application/json" },
 });
 
+// Request interceptor, add the token to the request header
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token'); // Or from your auth state
+  const token = localStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
@@ -14,5 +15,18 @@ api.interceptors.request.use((config) => {
 }, (error) => {
   return Promise.reject(error);
 });
+
+// Response interceptor, if the response status is 401
+// it means the token is invalid, so remove the token and redirect to the login page
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
